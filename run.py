17f159dbcd1dffe5f9c0032f7896ab4d9b6b1a52
Sunny Agarwal <sunny.agarwal@stablemoney.in>
2025-11-08 20:44:31 +0530

    remove debug

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ atexit.register(stop_worker)
 
 if __name__ == '__main__':
     try:
-        app.run(host='0.0.0.0', port=5001, debug=True)
+        app.run(host='0.0.0.0', port=5001, debug=False)
     except KeyboardInterrupt:
         print("\nShutting down gracefully...")
         stop_worker()
